526f729c8a95befdea47f4a7ce1d18d2978e0c05
skyiaccf <405319475@qq.com>
2022-06-29 19:33:27 +0800

    0629

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-print('Hello World')
-print('23333333333')
-print(233)
-print(12345678)
+## 这是一个注释
+"""
+这也是注释
+这也是注释
+"""
+
+
+txt = open('test.txt','w')
+txt.write('123456789')
+txt.close()
+
+txt = open('test.txt','r+')
+txt.write('00000')
+tttt = txt.readlines(txt,1)
+print(tttt)
+txt.close()
+
+
+
