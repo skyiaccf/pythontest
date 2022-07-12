099e1ee2bec82d536a298f4cd53ddbcb51d97141
skyiaccf <405319475@qq.com>
2022-07-12 22:12:18 +0800

    0712

--- test.py
+++ test.py
@@ -5,9 +5,17 @@
 """
 
 
+'''
 txt = open('test.txt','w')
 txt.write('人生苦短，我用Python')
 txt.close()
+'''
 
 
-
+str='ABCdefg'
+str=str.lower()
+print(str)
+print(str.split('e'))
+print(str.count('f'))
+print(str.find('e'))
+print(str.index('d'))
